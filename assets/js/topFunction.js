0074c3b6f1cb77b9be9b8c0ed45db44361c60997
unknown <tavig95@gmail.com>
2020-11-27 16:55:28 +0200

    fifth commit

--- assets/js/topFunction.js
+++ assets/js/topFunction.js
@@ -5,14 +5,15 @@ window.onscroll = function() {
 }
 
 function scrollFunction() {
-    if (document.body.ScrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.ScrollTop > 300 || document.documentElement.scrollTop > 300) {
         topButton.style.display = "block";
     }else {
         topButton.style.display = "none";
     }
 }
-
-function topFunction() {
+topButton.addEventListener("click", function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  });
+
+
