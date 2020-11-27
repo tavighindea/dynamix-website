08d13d08dad4be836bb91a56c5f1efcc560b8ea7
unknown <tavig95@gmail.com>
2020-11-27 15:14:25 +0200

    forth commit

---
+++ assets/js/topFunction.js
@@ -0,0 +1,18 @@
+const topButton = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    scrollFunction()
+}
+
+function scrollFunction() {
+    if (document.body.ScrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
