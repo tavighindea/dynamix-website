a1b0ed6f546d2c66c9a2cd15bcf8a5dc6cc1b2a9
unknown <tavig95@gmail.com>
2020-12-02 16:04:29 +0200

    with form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,7 +12,20 @@ form.addEventListener('submit', function(elem) {
     elem.preventDefault();
 
     if (checkInputs() === true) {
-        
+        const formData = new formData(form);
+        fetch(form.getAttribute('action'),{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: new URLSearchParams(formData).toString()
+        })
+        .then(function(res){
+            if (res) {
+                alert('worked');
+            }
+        });
     }
 })
 
