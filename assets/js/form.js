188dfaca9f9e452d69a0d42f75a65a17f20320e6
unknown <tavig95@gmail.com>
2020-12-02 13:50:40 +0200

    3rd try fix

--- assets/js/form.js
+++ assets/js/form.js
@@ -54,21 +54,3 @@ function isName(name) {
     return /^[a-zA-Z]+$/.test(name);
 }
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-  
-    const formData = new FormData(talkForm);
-    fetch(form.getAttribute('action'), {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-      },
-      body: new URLSearchParams(formData).toString()
-    })
-    .then(res => {
-      if (res) {
-          alert('worked');
-      }
-    });
-  });
