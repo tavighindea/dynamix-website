cd8ec396578f621558544aa683f3aad1753c5765
unknown <tavig95@gmail.com>
2020-12-09 01:42:32 +0200

    js form fix

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,9 +4,9 @@ const formTitle = document.getElementById('contact-title')
 const name = document.getElementById('name');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
-const nameError = document.getElementsByClassName('name-error inactive');
-const lastNameError = document.getElementsByClassName('last-name-error inactive');
-const emailError = document.getElementsByClassName('email-error inactive');
+const nameError = document.getElementById('name-error');
+const lastNameError = document.getElementById('last-name-error');
+const emailError = document.getElementById('email-error');
 
 
 testForm.addEventListener('submit', function(elem) {
